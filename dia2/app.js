f0ef7fb55d58799e3688642760176c4b4108b146
iSaul18 <porrasemmiliosaul@gmail.com>
2022-07-15 15:59:07 -0500

    dia 3

---
+++ dia2/app.js
@@ -0,0 +1,286 @@
+
+// ESTE DIA NO HUBO CLASES PERO PRACTIQUÉ TODOS LOS CODING CHALLENGUES DE JAVASCRIPT FUNDAMENTOS
+
+// Full Codings Callenges
+
+// JAVASCRIPT FUNDAMENTALS PARTE 1
+// Coding Challengue 1
+
+// // Test DATA 1
+// const markMass = 78;
+// const markHeitht = 1.69;
+// const jhonMass = 92;
+// const jhonHeight = 1.95;
+
+// const markBMI = markMass / markHeitht **2;
+// const jhonBMI = jhonMass / (jhonHeight * jhonHeight);
+// const markHigherBMI = markBMI > jhonBMI;
+
+// console.log(`Mark(${markBMI}) vs Jhon(${jhonBMI})`);
+// console.log(markHigherBMI)
+
+
+// Test DATA 2
+// const markMass = 95;
+// const markHeitht = 1.88;
+// const jhonMass = 85;
+// const jhonHeight = 1.76;
+
+// const markBMI = markMass / markHeitht **2;
+// const jhonBMI = jhonMass / (jhonHeight * jhonHeight);
+
+// console.log(`Mark(${markBMI}) vs Jhon(${jhonBMI})`);
+
+// *******************************************
+
+// JAVASCRIPT FUNDAMENTALS PARTE 1
+// Coding Challenge 2
+
+// // Test DATA 1
+// const markMass = 78;
+// const markHeitht = 1.69;
+// const jhonMass = 92;
+// const jhonHeight = 1.95;
+
+// const markBMI = markMass / markHeitht **2;
+// const jhonBMI = jhonMass / (jhonHeight * jhonHeight);
+// const markHigherBMI = markBMI > jhonBMI;
+
+// console.log(`Mark(${markBMI}) vs Jhon(${jhonBMI})`);
+
+// if (markBMI < jhonBMI) {
+//   console.log(`Jhon tiene más masa corporal que Mark`);
+// } else if (markBMI > jhonBMI){
+//   console.log(`Mark tiene más masa corporal que jhon`);
+// } else {
+//   console.log(`Mark y Jhon tienen la misma masa corporal`);
+// }
+
+// // Test DATA 2
+// const markMass = 95;
+// const markHeitht = 1.88;
+// const jhonMass = 85;
+// const jhonHeight = 1.76;
+
+// const markBMI = markMass / markHeitht **2;
+// const jhonBMI = jhonMass / (jhonHeight * jhonHeight);
+// const markHigherBMI = markBMI > jhonBMI;
+
+// console.log(`Mark(${markBMI}) vs Jhon(${jhonBMI})`);
+
+// if (markBMI < jhonBMI) {
+//   console.log(`Jhon tiene más masa corporal que Mark`);
+// } else if (markBMI > jhonBMI){
+//   console.log(`Mark tiene más masa corporal que jhon`);
+// } else {
+//   console.log(`Mark y Jhon tienen la misma masa corporal`);
+// }
+
+// *******************************************
+
+// JAVASCRIPT FUNDAMENTALS PARTE 1
+// Coding Challenge 3
+
+// Test Data
+
+// let teamDolphins = (96 + 108 + 89) / 3;
+// let teamKoalas = (88 + 91 + 110) / 3;
+
+// console.log(`teamDolphins(${teamDolphins}) vs teamKoalas(${teamKoalas})`);
+
+// // Agregar minin score of 100 
+
+// if (teamDolphins > teamKoalas && teamDolphins >= 100) {
+//   console.log(`teamDolphins Win`);
+
+// }else if (teamDolphins < teamKoalas && teamDolphins >= 100) {
+//   console.log(`teamKoalas Win`);
+
+// } else if (){
+//   console.log(`Ambos equipo empataron`);
+
+// } else {
+//   console.log(`Ningun equipo ganó`);
+// }
+
+// // Test Data 2
+
+// teamDolphins = (97 + 112 + 101) / 3;
+// teamKoalas = (109 + 95 + 123) / 3;
+
+// console.log(`teamDolphins(${teamDolphins}) vs teamKoalas(${teamKoalas})`);
+
+// // Agregar minin score of 100 
+// if (teamDolphins > teamKoalas && teamDolphins >= 100) {
+//   console.log(`teamDolphins Win`);
+
+// }else if (teamDolphins < teamKoalas && teamDolphins >= 100) {
+//   console.log(`teamKoalas Win`);
+
+// } else if (){
+//   console.log(`Ambos equipo empataron`);
+  
+// } else {
+//   console.log(`Ningun equipo ganó`);
+// }
+
+// // Test Data 3
+
+// teamDolphins = (97 + 112 + 101) / 3;
+// teamKoalas = (109 + 95 + 123) / 3;
+
+// console.log(`teamDolphins(${teamDolphins}) vs teamKoalas(${teamKoalas})`);
+
+// // Agregar minin score of 100 
+// if (teamDolphins > teamKoalas && teamDolphins >= 100) {
+//   console.log(`teamDolphins Win`);
+
+// }else if (teamDolphins < teamKoalas && teamDolphins >= 100) {
+//   console.log(`teamKoalas Win`);
+
+// } else if (){
+//   console.log(`Ambos equipo empataron`);
+  
+// } else {
+//   console.log(`Ningun equipo ganó`);
+// }
+
+// **********************************************************
+
+// JAVASCRIPT FUNDAMENTALS PARTE 1
+
+// Challenge 4
+
+// Test data
+
+// const bill = 275;
+// const tip = 50 <= bill <= 300? bill * 0.15 : bill * 0.2 ;
+// const total = `The bill was ${bill}, the tip was as ${tip}, and the total value ${bill + tip}`;
+
+// console.log(total);
+
+
+//**********************************************************
+
+// JAVASCRIPT FUNDAMENTALS PARTE 2
+
+// CODING CHALLENGE 1
+
+// Testa data 1
+
+// const calAverage = (a, b, c) => (a + b + c) / 3;
+
+// const promTeamDolphins = calAverage(44, 23, 71);
+// const promTeamKoalas = calAverage(65, 54, 49)
+
+// console.log(`Team Dolphins(${promTeamDolphins}) vs Team Koalas(${promTeamKoalas})`);
+
+// if (promTeamDolphins > promTeamKoalas && promTeamDolphins > promTeamKoalas *2) {
+//   console.log(`Team dolphins win`);
+// }else if(promTeamDolphins < promTeamKoalas && promTeamKoalas > promTeamDolphins *2){
+//   console.log(`Team Koalas Win`);
+// } else {
+//   console.log(`Ningun equipo ganó`);
+// }
+
+// Testa data 2
+
+// const calAverage = (a, b, c) => (a + b + c) / 3;
+
+// const promTeamDolphins = calAverage(85, 54, 41);
+// const promTeamKoalas = calAverage(23, 43, 27)
+
+// console.log(`Team Dolphins(${promTeamDolphins}) vs Team Koalas(${promTeamKoalas})`);
+
+// if (promTeamDolphins > promTeamKoalas && promTeamDolphins > promTeamKoalas *2) {
+//   console.log(`Team dolphins win`);
+// }else if(promTeamDolphins < promTeamKoalas && promTeamKoalas > promTeamDolphins *2){
+//   console.log(`Team Koalas Win`);
+// } else {
+//   console.log(`Ningun equipo ganó`);
+// }
+
+// ****************************************
+
+// JAVASCRIPT FUNDAMENTALS PARTE 2
+
+// CODING CALLENGE 2
+
+// Test data
+
+// const caltip = (bill) => 50 <= bill <= 300? bill * 0.15 : bill * 0.2;
+
+// const bills = [125, 55, 44];
+// const tips = [caltip(bills[0]), caltip(bills[1], caltip(bills[2]))];
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+
+// **********************************************
+
+
+// JAVASCRIPT FUNDAMENTALS PARTE 2
+
+// CODING CALLENGE 3
+
+// const mark = {
+//   mass: 78,
+//   height: 1.69,
+//   calBMI: function() {
+//     return this.BMI = this.mass / this.height ** 2;
+//   }
+// };
+
+// const jhon = {
+//   mass: 92,
+//   height: 1.95,
+//   calBMI: function() {
+//     return this.BMI = this.mass / this.height ** 2;
+//   }
+// };
+
+// console.log(`Mark tiene un BMI de: (${mark.calBMI()}) vs Jhon que tiene un BMI de: (${jhon.calBMI()})`);
+
+// if (jhon.BMI > mark.BMI) {
+//   console.log(`El BMI de Jhon es mayor que el de Mark`);
+// } else if (jhon.BMI < mark.BMI) {
+//   console.log(`El BMI de Mark es mayor que el de Jhon`);
+// } else {
+//   console.log(`El BMI de Mark y el de Jhon son iguales`);
+// }
+
+// ******************************************
+
+// JAVASCRIPT FUNDAMENTALS PARTE 2
+
+// CODING CALLENGE 4
+
+// const caltip = function(bills) {
+//   const tip = 50 <= bills <= 300? bills * 0.15 : bills * 0.2;
+//   return tip;
+// }; 
+
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
+
+// for (let repeat = 0 ; repeat < bills.length ; repeat ++) {
+  
+//   const inserTip = caltip(bills[repeat]);
+//   tips.push(inserTip)
+
+//   const inserTotal = inserTip + bills[repeat];
+//   totals.push(inserTotal)
+// };
+
+
+// const calAverage = function (arr) {
+//   let suma = 0;
+
+//   for (let repeat = 0 ; repeat < arr.length ; repeat ++) {
+//     suma += arr[repeat];
+//   }
+//   return suma / arr.length;
+// }
+
+// console.log(`Promedio de Bills: (${calAverage(bills)})`);
+// console.log(`Promedio de Tips: (${calAverage(tips)})`);
+// console.log(`Promedio de Totals: (${calAverage(totals)})`);
